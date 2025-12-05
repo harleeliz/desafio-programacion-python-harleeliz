6a8bc4970788a561c51137fa3f33d01248d94aa4
Harlee Ramos <145159465+harleeliz@users.noreply.github.com>
2025-12-05 22:48:48 +0000

    update

--- 01_01_datos_faltantes/01_01_datos_faltantes.py
+++ 01_01_datos_faltantes/01_01_datos_faltantes.py
@@ -1,7 +1,21 @@
+#En este file copio los codigos del video, mi version es el notebook
+import csv
 import pandas as pd
-df = pd.read_csv('empleados.csv', delimiter=";")
 
+
+#Leyendo path para evitar no ubicar el file
+dir(csv)
+path= "/workspaces/desafio-programacion-python-harleeliz/01_01_datos_faltantes/empleados.csv"
+df = pd.read_csv(path, delimiter=";")
 print(df.head(20))
 print(df.shape)
 print(df.isnull().sum())
 print(df.notnull().sum())
+print(df[df.isnull().any(axis=1)])
+df_act=df.drop(axis=1,columns=['Apellido'])
+df_actualizado=df_act.dropna(axis=0)
+print(df_actualizado)
+
+#Eliminacion de algunas files que tienen al menos 5 filas no validas
+df_actualizado=df_act.dropna(axis=0,thresh=5)
+
